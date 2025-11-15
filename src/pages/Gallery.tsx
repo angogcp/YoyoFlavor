b72203da5ed4c0f588b24445005b383329aa3649
ango <angogcp@gmail.com>
2025-11-15 16:54:36 +0800

    chore(gallery): remove menu, logo, banner, and hours assets from gallery carousel

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -5,28 +5,15 @@ import Page from '../components/Page'
 const images = [
   '/images/1763179965.png',
   '/images/Beer_&_Snack_Comfort_version_1.png',
-  '/images/Beveraage.jpg',
-  '/images/Bread and Wok fried.jpg',
   '/images/Flavor_&_Friends_version_1 (1).png',
   '/images/Flavor_&_Friends_version_1.png',
   '/images/Flavor_Is_Just_Around_the_Corner_version_1.png',
-  '/images/HotPot.jpg',
   '/images/Indulgence_Is_on_the_Menu_version_1.png',
   '/images/Need_a_Moment_to_Reset__version_1.png',
-  '/images/Noodles and rice.jpg',
   '/images/Open_Air_Game_Day_Comfort_version_1.png',
-  '/images/Snacks.jpg',
-  '/images/Western.jpg',
   '/images/Your_Daily_Escape_Starts_Here__version_1.png',
   '/images/Your_Daily_Escape__Right_Here__version_1.png',
-  '/images/japanese flav.jpg',
-  '/images/japanese flav2.jpg',
-  '/images/yoyo hour.png',
-  '/images/yoyo intro.png',
-  '/images/yoyo logo.jpg',
-  '/images/yoyo logo.png',
-  '/images/yoyo-new-.png',
-  '/images/yoyoBanner.png'
+  '/images/yoyo intro.png'
 ]
 
 export default function Gallery() {
